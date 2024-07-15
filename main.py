aca9f1fe811c8c397df452c5940e15eb2039f456
Anthony Su <anthonysu798.ca@gmail.com>
2024-07-15 16:25:12 -0400

    Added Select Search Bar Element

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 import time
 
 service = Service(excutable_path="chromedriver.exec")
 driver = webdriver.Chrome(service=service)
 
 driver.get("https://google.com")
+
+# Get element className
+input_element = driver.get_element(By.CLASS_NAME, "gLFyf")
+
+# Now send the key to that element
+input_element.send_keys("Tech with Anthony Su")
 
 time.sleep(10) # Sleep for 10 sec
 
